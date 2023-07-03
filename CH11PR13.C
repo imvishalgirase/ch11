3efdf1bc23d599c2b2c7e2ce06e433c3668c87c1
imvishalgirase <137286597+imvishalgirase@users.noreply.github.com>
2023-07-03 05:29:58 -0700

    Add files via upload

---
+++ CH11PR13.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int i,n;
+   clrscr();
+   printf("Plz enetr the value of n=");
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+      printf("%d\n",i);
+
+
+      }
+   getch();
+ }
---
+++ CH11PR14.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int i,n;
+  clrscr();
+  printf("Plz enter the first value=");
+  scanf("%d",&n);
+
+ for(i=n;i>=0;i--)
+  {
+      printf("%d\n",i);
+
+    }
+       //	 for(i=n ; i>= ; i--){
+	  //  printf("%d\n",i);
+      //	 }
+	getch();
+	}
